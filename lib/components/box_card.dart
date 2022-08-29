056f81f898236a70b93c31a028a9f441bb63ebd4
ikyrie <mah.c5r@gmail.com>
2022-08-29 19:48:10 -0300

    vídeo 3.3

--- lib/components/box_card.dart
+++ lib/components/box_card.dart
@@ -7,6 +7,11 @@ const BoxCard({ Key? key, required this.boxContent }) : super(key: key);
   @override
   Widget build(BuildContext context){
     return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        boxShadow: kElevationToShadow[3],
+        borderRadius: BorderRadius.circular(10),
+      ),
       child: boxContent,
     );
   }
