78754542115b386d152f2f8e2a04e7c49cfcda75
ikyrie <mah.c5r@gmail.com>
2022-08-29 18:44:16 -0300

    vídeo 1.4

---
+++ lib/components/sections/header.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+const Header({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+            Text('\$1000.00'),
+            Text('Balanço disponível'),
+          ],),
+          Icon(Icons.account_circle),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,11 @@
+import 'package:alubank/components/sections/header.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+const Home({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(body: Header(),);
+  }
+}
