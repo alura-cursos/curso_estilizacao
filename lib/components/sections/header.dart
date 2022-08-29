38aa8ccf5efa73cc1cd5841a916711f1651c4cf0
ikyrie <mah.c5r@gmail.com>
2022-08-29 18:55:40 -0300

    vídeo 2.2

--- lib/components/sections/header.dart
+++ lib/components/sections/header.dart
@@ -6,17 +6,20 @@ const Header({ Key? key }) : super(key: key);
   @override
   Widget build(BuildContext context){
     return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-            Text('\$1000.00'),
-            Text('Balanço disponível'),
-          ],),
-          Icon(Icons.account_circle),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16.0, 80.0, 16.0, 16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+              Text('\$1000.00', style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),),
+              Text('Balanço disponível', style: TextStyle(fontSize: 16),),
+            ],),
+            Icon(Icons.account_circle, size: 42,),
+          ],
+        ),
       ),
     );
   }
