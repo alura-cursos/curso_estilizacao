4c6d06934d7df134cb3bf4cd8667c5fd030d863e
ikyrie <mah.c5r@gmail.com>
2022-09-01 15:50:21 -0300

    vídeo 5.4

--- lib/components/sections/recent_activity.dart
+++ lib/components/sections/recent_activity.dart
@@ -1,5 +1,6 @@
 import 'package:alubank/components/box_card.dart';
 import 'package:alubank/components/color_dot.dart';
+import 'package:alubank/components/content_division.dart';
 import 'package:alubank/themes/theme_colors.dart';
 import 'package:flutter/material.dart';
 
@@ -21,8 +22,10 @@ class _RecentActivityContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Row(
               children: [
@@ -39,8 +42,45 @@ class _RecentActivityContent extends StatelessWidget {
                 ),
               ],
             ),
-            Row(),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 4.0),
+                  child: ColorDot(color: ThemeColors.recentActivity['income']),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Entrada'),
+                    Text('\$9900.97', style: Theme.of(context).textTheme.bodyLarge),
+                  ],
+                ),
+              ],
+            ),
           ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
+          child: Text('Limite de gastos: \$432.90'),
+        ),
+        Container(
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: LinearProgressIndicator(
+            value: 0.3,
+            minHeight: 8,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+          child: ContentDivision(),
+        ),
+        Text('Esse mês você gastou \$1500.00 com jogos. Tente abaixar esse custo!'),
+        TextButton(
+          onPressed: () {},
+          child: Text('Diga-me como!', style: TextStyle(fontSize: 16),),
         ),
       ],
     );
